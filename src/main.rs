d48b906c36c93a00902378062d5f9075359edc31
Ian Teda <ian@teda.id.au>
2024-06-23 08:11:59 +1000

    Initial TUI

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 //-- ./src/main.rs
 
+#![allow(unused)] // For beginning only.
+
 use crate::prelude::*;
 use std::fs::read_dir;
 
@@ -7,16 +9,46 @@ mod error;
 mod prelude;
 mod utils;
 
-// #![allow(unused)] // For beginning only.
+use crossterm::{
+    event::{self, KeyCode, KeyEventKind},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::{
+    prelude::{CrosstermBackend, Stylize, Terminal},
+    widgets::Paragraph,
+};
+use std::io::{stdout, Result};
+
 
 /// Binary entry point
 fn main() -> Result<()> {
-	println!("Hello, world!");
+	stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+    terminal.clear()?;
 
-	for entry in read_dir("./")?.filter_map(|e| e.ok()) {
-		let entry: String = W(&entry).try_into()?;
-		println!("{entry}");
-	}
+	loop {
+		terminal.draw(|frame| {
+            let area = frame.size();
+            frame.render_widget(
+                Paragraph::new("Hello Ratatui! (press 'q' to quit)")
+                    .white()
+                    .on_blue(),
+                area,
+            );
+        })?;
+		if event::poll(std::time::Duration::from_millis(16))? {
+            if let event::Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                    break;
+                }
+            }
+        }
+    }
+
+    stdout().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
 
 	Ok(())
 }
